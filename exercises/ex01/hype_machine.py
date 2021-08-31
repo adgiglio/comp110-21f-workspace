ee9fea7ba432d4022a4789ec01500e821d0fc724
Alexandra Giglio <adgiglio@live.unc.edu>
2021-08-30 23:52:46 -0400

    finished EX01

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,13 @@
 # TODO: Write docstring here
-
+"""A program to gas you up."""
 # TODO: Initialize __author__ variable here
+__author__ = "730313501"
 
 # TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print("You entered: ")
+print(name)
+
+print(name + ", you are the greatest coder ever!")
+print("You can do it, " + name + "!")
+print("You're gonna KILL it, " + name + ", you got this!")
---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,11 @@
+"""Practicing numeric operators, type conversions, and string concatenation."""
+
+__author__ = "730313501"
+
+left_hand_side: int = int(input("Left-hand side: "))
+right_hand_side: int = int(input("Right-hand side: "))
+
+print(str(left_hand_side) + " ** " + str(right_hand_side) + " is " + str(left_hand_side ** right_hand_side))
+print(str(left_hand_side) + " / " + str(right_hand_side) + " is " + str(left_hand_side / right_hand_side))
+print(str(left_hand_side) + " // " + str(right_hand_side) + " is " + str(left_hand_side // right_hand_side))
+print(str(left_hand_side) + " % " + str(right_hand_side) + " is " + str(left_hand_side % right_hand_side))
